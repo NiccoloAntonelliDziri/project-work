fa51ec56612182457cca0a877e687cdd70c9373e
Niccolo Antonelli-Dziri <niccolo.antonelli-dziri@protonmail.com>
2026-01-21 14:40:03 +0100

    Update to the formatting path conversion to include nodes where no gold is picked up

--- s350296.py
+++ s350296.py
@@ -1,6 +1,7 @@
 from Problem import Problem, compute_total_cost, is_valid_solution
 from src.genetic_algorithm import GeneticAlgorithmSolver
 import numpy as np
+from scipy.sparse import csgraph
 from icecream import ic
 from src.plot import plot_solution, plot_score_log
 
@@ -8,28 +9,67 @@ def to_formatted_path(routes, problem):
     """
     Converts a list of routes (each starting and ending with 0)
     into a single sequential path of (node, gold_collected) tuples.
+    This version includes all intermediate nodes traversed in the shortest path.
     Format: [(c1, g1), (c2, g2), ..., (cN, gN), (0, 0)]
     """
+    dist_matrix = problem.distance_matrix()
+    dist_matrix = np.where(np.isinf(dist_matrix), 0, dist_matrix)
+    _, predecessors = csgraph.shortest_path(dist_matrix, directed=False, return_predecessors=True)
+    
     formatted_path = []
+    # Set to keep track of nodes where gold has been picked up
+    visited_for_gold = set()
+    
     for route in routes:
-        # Each route starts with depot (0) and ends with depot (0). 
-        # We skip the first 0 of each route to concatenate them.
-        for node in route[1:]:
-            gold = problem.graph.nodes[node]['gold']
-            formatted_path.append((node, gold))
+        for i in range(len(route) - 1):
+            from_node = route[i]
+            to_node = route[i + 1]
+            
+            # If to_node was already visited (for gold collection) and it's not the depot, 
+            # we skip this segment according to the cost calculation logic.
+            if to_node in visited_for_gold and to_node != 0:
+                continue
+            
+            # Mark the node as visited for gold collection
+            if to_node != 0:
+                visited_for_gold.add(to_node)
+            
+            # Reconstruct the shortest path from from_node to to_node
+            path_segment = []
+            curr = to_node
+            while curr != from_node:
+                path_segment.append(curr)
+                prev = predecessors[from_node, curr]
+                if prev < 0: # Should not happen in a connected graph
+                    break
+                curr = prev
+            path_segment.reverse()
+            
+            # Add nodes in the path to the formatted path
+            for node in path_segment:
+                gold = 0
+                # We only "pick up" gold at the destination node of this segment
+                if node == to_node and node != 0:
+                    gold = problem.graph.nodes[node]['gold']
+                formatted_path.append((node, gold))
+                
     return formatted_path
 
 def to_routes(formatted_path):
     """
     Converts a sequential path of (node, gold_collected) tuples
     back into a list of routes (each starting and ending with 0).
+    Filters out intermediate nodes that didn't collect gold to restore checkpoints.
     Input Format: [(c1, g1), (c2, g2), ..., (cN, gN), (0, 0)]
     Output Format: [[0, c1, ..., cN, 0], [0, ...], ...]
     """
     routes = []
     current_route = [0]
-    for node, _ in formatted_path:
-        current_route.append(node)
+    for node, gold in formatted_path:
+        # Keep nodes where gold was collected or the depot
+        if gold > 0 or node == 0:
+            current_route.append(node)
+        
         if node == 0:
              if len(current_route) > 1:
                  routes.append(current_route)
@@ -48,12 +88,16 @@ def solution(problem: Problem):
     
     routes, _, score_log = solver.run()
 
-    print("Plotting logs...")
-    plot_score_log(score_log, filename="ga_score_log.png", log_scale=True)
-    print("Converting to formatted path of the solution...")
     formatted_path = to_formatted_path(routes, problem)
-    print("Plotting solution...")
-    plot_solution(problem, formatted_path, filename="ga_solution.png")
+
+    # Uncomment the following lines for plots
+
+    # print("Plotting logs...")
+    # plot_score_log(score_log, filename="ga_score_log.png", log_scale=True)
+    # print("Converting to formatted path of the solution...")
+
+    # print("Plotting solution...")
+    # plot_solution(problem, formatted_path, filename="ga_solution.png")
 
     return formatted_path
 
